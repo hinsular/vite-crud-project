2d7b68b05a233856c60e16993843b9a8bacbc584
hinsular <honeylene.insular@edu.sait.ca>
2023-09-02 15:28:15 -0600

    added table

---
+++ info_table.sql
@@ -0,0 +1,18 @@
+INSERT INTO posts (
+firstName, 
+lastName, 
+birthday, 
+address, 
+email, 
+phoneNumber, 
+postcol)
+
+VALUES (
+"Catherine",
+"McBride",
+1996-08-09,
+"120 Sunset Valley Simston",
+"cathmcbride@starlite.com",
+587-889-1092,
+"T32 5G5"
+)
